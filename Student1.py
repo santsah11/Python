6810056e21b7ffb23a5310cdac960bddb5741152
santsah11 <santsah.np@gmail.com>
2017-11-03 14:21:02 -0700

    Add files via upload
    This about the Dictionary is inside dictionary,

---
+++ Student1.py
@@ -0,0 +1,11 @@
+students = {
+            "Alice":{"id":"ID101","age":27,"grade":"A"},
+            "Bob": {"id":"ID102","age":25,"grade":"B"},
+            "Claire":{"id":"ID1003","age":35,"grade":"C"},
+            "Dan":{"id":"ID104","age":30,"grade":"E"},
+            "Emma":{"id":"ID105","age":26,"grade":"C"}
+
+            }
+
+print(students["Dan"]["id"])
+print(students["Emma"]["id"],students["Emma"]["age"])
